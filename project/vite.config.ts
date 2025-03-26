b3e320258151059f423ac5f1b7ee378ad0aa75f0
senthoo <senthooran.thayaparan.pro@gmail.com>
2025-03-26 23:43:53 +0100

    fezf

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PFSENTHOO/Home',
+  base: '/PFSENTHOO/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
